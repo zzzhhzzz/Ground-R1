4293fe8ed5b81432799f25bfcb6883312244ab1b
zzzhhzzz <zeze2002@126.com>
2025-06-10 12:08:05 +0000

    fix

--- r1-v/setup.py
+++ r1-v/setup.py
@@ -53,7 +53,7 @@ _deps = [
     "huggingface-hub[cli]>=0.19.2,<1.0",
     "isort>=5.12.0",
     "liger_kernel==0.5.2",
-    "lighteval @ git+https://github.com/huggingface/lighteval.git@4f381b352c0e467b5870a97d41cb66b487a2c503#egg=lighteval[math]",
+    "lighteval",
     "math-verify",  # Used for math verification in grpo
     "packaging>=23.0",
     "parameterized>=0.9.0",
@@ -61,7 +61,7 @@ _deps = [
     "safetensors>=0.3.3",
     "sentencepiece>=0.1.99",
     "torch>=2.5.1",
-    "transformers @ git+https://github.com/huggingface/transformers.git@336dc69d63d56f232a183a3e7f52790429b871ef",
+    "transformers",
     "trl==0.14.0",
     "vllm==0.6.6.post1",
     "wandb>=0.19.1",
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Install the packages in r1-v .
-cd src/r1-v 
+cd r1-v 
 pip install -e ".[dev]"
 
 # Addtional modules
